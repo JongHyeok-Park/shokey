2c563648871a880ab31533ac65b12cb0d4f19490
hj <hanguswls@knu.ac.kr>
2024-08-01 01:03:03 +0900

    style: 로그인 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Register from './routes/Register'
+import Login from './routes/Login'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
       <Footer />
     </div>
