3d8a53f77862475ea6c3beba039107a139e86bd9
hj <hanguswls@knu.ac.kr>
2024-08-01 10:14:34 +0900

    style: Footer 배경 색상 채움

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,26 +4,28 @@ import logo from '/public/logo.png';
 export default function Footer() {
   return (
     <footer>
-      <section className="footer-top">
-        <div className="logo">
-          <img src={logo} alt="logo" />
-          <span>
-            SHOKEY
+      <div className='footer-wrapper'>
+        <section className="footer-top">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <span>
+              SHOKEY
+            </span>
+          </div>
+          <span id="shokey-description">
+            Join millions of people who organize work and life with SHOKEY.
           </span>
-        </div>
-        <span id="shokey-description">
-          Join millions of people who organize work and life with SHOKEY.
-        </span>
-      </section>
-      <hr />
-      <section className="footer-bottom">
-        <span id="copyright">
-          © SHOKEY - Design by Hoang Kim Quoc
-        </span>
-        <a id="terms" href="/terms">
-          약관
-        </a>
-      </section>
+        </section>
+        <hr />
+        <section className="footer-bottom">
+          <span id="copyright">
+            © SHOKEY - Design by Hoang Kim Quoc
+          </span>
+          <a id="terms" href="/terms">
+            약관
+          </a>
+        </section>
+      </div>
     </footer>
   )
 }
