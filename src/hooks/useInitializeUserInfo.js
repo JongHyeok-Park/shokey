1311eec25ecd23d91b5ace3baac679b927c95226
hj <hanguswls@knu.ac.kr>
2024-08-06 17:02:04 +0900

    fix: 중복 코드로 인한 오류 제거

--- src/hooks/useInitializeUserInfo.js
+++ src/hooks/useInitializeUserInfo.js
@@ -24,35 +24,3 @@ const useInitializeUserInfo = () => {
     }
   };
 };
-
-export default useInitializeUserInfo;
-import { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
-import { getMyInfo } from '../apis/userInfoApi';
-import useUserStore from '../store/userStore';
-
-/** accessToken이 바뀔 때 id, userName을 받아와 전역상태로 저장*/
-const useInitializeUserInfo = () => {
-  const [cookies] = useCookies(['accessToken']);
-  const { setId, setUserName, clearUser } = useUserStore();
-
-  useEffect(() => {
-    initializeUserInfo();
-  }, [cookies.accessToken]);
-
-  const initializeUserInfo = async () => {
-    if (cookies.accessToken) {
-      try {
-        const res = await getMyInfo(cookies.accessToken);
-        setId(res.data.id);
-        setUserName(res.data.userName);
-      } catch (error) {
-        alert(error.message);
-        clearUser();
-      }
-    }
-    clearUser();
-  };
-};
-
-export default useInitializeUserInfo;
