8360c8c7deb0a3219d45abb40f90fe42eb4c0bab
hj <hanguswls@knu.ac.kr>
2024-08-06 20:46:50 +0900

    feat: 등록되지 않은 사용자의 페이지를 방문할 경우 '사용자 정보 없음' 표시

--- src/routes/ViewUserInfo.jsx
+++ src/routes/ViewUserInfo.jsx
@@ -9,21 +9,30 @@ const ViewUserInfo = () => {
 
   return (
     <div className="user-info-container">
-      <h2 className="title">사용자 정보 조회</h2>
+    <h2 className="title">사용자 정보 조회</h2>
+    {(userInfo.userName === '') ?
+      <h2 id="no-user-info">사용자 정보 없음</h2> :
       <div className="fields">
         <div className="field">
           <span className="field-name">이름</span>
-          <span className="field-value">{userInfo.userName}</span>
+          <span className="field-value">
+            {userInfo.userName}
+          </span>
         </div>
         <div className="field">
           <span className="field-name">성별</span>
-          <span className="field-value">{userInfo.userGender ? '남성' : '여성'}</span>
+          <span className="field-value">
+            {userInfo.userGender ? '남성' : '여성'}
+          </span>
         </div>
         <div className="field">
           <span className="field-name">이메일</span>
-          <span className="field-value">{userInfo.userEmail}</span>
+          <span className="field-value">
+            {userInfo.userEmail}
+          </span>
         </div>
       </div>
+    }
     </div>
   );
 };
