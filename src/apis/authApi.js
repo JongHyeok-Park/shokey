ffcfcc883dbd4777dadfe1cfba9c2e5d27670847
Jonghyeok <jhpmuscle@gmail.com>
2024-08-19 10:38:22 +0900

    feat: 본인 정보 조회 api

--- src/apis/authApi.js
+++ src/apis/authApi.js
@@ -56,4 +56,19 @@ const postRefresh = async (refreshToken) => {
 	return res.json();
 }
 
+const getMyInfo = async (token) => {
+	const res = await fetch(import.meta.env.VITE_APP_API_URL + '/api/me', {
+		headers: {
+			'authorization': 'Bearer ' + token
+		}
+	});
+
+	if (!res.ok) {
+		const message = (await res.json()).statusMsg;
+		throw new Error(message);
+	}
+
+	return res.json();
+}
+
 export { postRegister, postLogin, postRefresh };
